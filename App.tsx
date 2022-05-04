ade43d0ca8b6a04534053050c68006151cc5dbdd
geanderson062 <83518241+geanderson-062@users.noreply.github.com>
2022-05-04 15:58:05 -0300

    criando o componente preco atual

--- App.tsx
+++ App.tsx
@@ -1,13 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import react from 'react';
+import { StatusBar } from 'react-native';
 import { View } from 'react-native';
 import { AppRoutes } from './src/router/app.routes';
+
+//backgroundColor cor da barrado superior com os icones
+//barStyle cor dos icones na barra superior
 
 export default function App() {
   return (
 
     <View style={{flex: 1}}>
-            <StatusBar style="auto" />
+            <StatusBar 
+             backgroundColor="#fff" 
+             barStyle="dark-content" />
             <AppRoutes/>
     </View>
 
--- src/pages/Grafico.tsx
+++ src/pages/Grafico.tsx
@@ -2,12 +2,17 @@
 import React from 'react'
 import {View} from 'react-native'
 
+//importando o componente
+import Preco_atual from '../components/Preco_atual'
+
 export function Grafico() {
   
     return (
 
         <View style={{ flex: 1 , backgroundColor: '#303030'}}>
 
+          <Preco_atual/>
+ 
         </View>
     )
     
