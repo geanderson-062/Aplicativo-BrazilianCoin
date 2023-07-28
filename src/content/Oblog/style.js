b40e9314e1379dcf4488995ec24f8b600b3e9dea
geanderson062 <83518241+geanderson-062@users.noreply.github.com>
2023-07-28 15:26:20 -0300

    screenshots

--- src/content/Oblog/style.js
+++ src/content/Oblog/style.js
@@ -1,16 +1,17 @@
-import {  StyleSheet } from 'react-native'
+import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
+  caRd: {
+    //aqui fica cabecalho ele vai alinhar tudo
 
-    caRd : {  //aqui fica cabecalho ele vai alinhar tudo 
-
-        width:"90%", //largura do grafico
-        height: "50%",//altura fixa
-        backgroundColor: "#232323",//cor do fundo do grafico
-        borderRadius: 10,//borda arredondada
-        marginTop: 30,
-    },
-
+    width: "90%", //largura do grafico
+    height: "55%", //altura fixa
+    backgroundColor: "#232323", //cor do fundo do grafico
+    borderRadius: 10, //borda arredondada
+    marginTop: 30,
+    marginLeft: 12,
+    marginRight: 12,
+  },
 });
 
-export default styles
+export default styles;
